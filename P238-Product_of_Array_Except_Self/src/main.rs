a42400cd3e6d1548330a8bfc30e642b4d4ea0f6d
Ellipsis-Fr <crespel.romain@gmail.com>
2024-07-03 23:04:24 +0200

    P238 resolved

--- P238-Product_of_Array_Except_Self/src/main.rs
+++ P238-Product_of_Array_Except_Self/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 fn main() {
 }
 
@@ -5,7 +7,27 @@ struct Solution;
 
 impl Solution {
     pub fn product_except_self(nums: Vec<i32>) -> Vec<i32> {
-        vec![]
+        let mut result = Vec::new();
+        let mut cache = HashMap::new();
+
+        for (index, num) in nums.iter().enumerate() {
+            if let Some(value) = cache.get(num) {
+                result.push(*value);
+            } else {
+                let mut nums_without_self = nums.clone();
+                nums_without_self.remove(index);
+                if nums_without_self.contains(&0) {
+                    result.push(0);
+                } else {
+                    let product = nums_without_self.iter().product();
+                    result.push(product);
+                    cache.insert(num, product);
+                }
+            }
+        }
+
+
+        result
     }
 }
 
@@ -17,7 +39,16 @@ mod tests {
     #[test]
     fn test_1() {
         let input = vec![1,2,3,4];
+        let output = Solution::product_except_self(input);
 
-        assert_eq!(vec![1,2,3,4], input);
+        assert_eq!(vec![24,12,8,6], output);
+    }
+
+    #[test]
+    fn test_2() {
+        let input = vec![-1,1,0,-3,3];
+        let output = Solution::product_except_self(input);
+
+        assert_eq!(vec![0,0,9,0,0], output);
     }
 }
