dd7f88186c3395f4e98e0b697de0c672b02fd41d
Ellipsis-Fr <crespel.romain@gmail.com>
2024-07-03 22:26:55 +0200

    init P238

---
+++ P238-Product_of_Array_Except_Self/src/main.rs
@@ -0,0 +1,23 @@
+fn main() {
+}
+
+struct Solution;
+
+impl Solution {
+    pub fn product_except_self(nums: Vec<i32>) -> Vec<i32> {
+        vec![]
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::Solution;
+
+
+    #[test]
+    fn test_1() {
+        let input = vec![1,2,3,4];
+
+        assert_eq!(vec![1,2,3,4], input);
+    }
+}
